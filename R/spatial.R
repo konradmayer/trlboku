7a23c29b319d438f69473d0a3772c1b76b10a174
konradmayer <konrad.mayer@boku.ac.at>
2016-10-14 15:48:45 +0200

    rename new_epsg to other_projection, update documentation and basic input checks

--- R/spatial.R
+++ R/spatial.R
@@ -1,17 +1,29 @@
 #------------------------------
-#new_epsg
+#other_projection
 #------------------------------
-#' @title new_epsg
-#' @description Set new proj4string by supplying a EPSG code.
+#' @title transform_projection
+#' @description Transforms to different projection by supplying an EPSG code.
+#'   If object has no proj4string defined, old projection can be specified by
+#'   argument epsg_old.
 #'
+#'   EPSG of common projections:
+#'   \itemize{
+#'     \item{WG84: 4326}
+#'     \item{Lambert Austria: 31287}
+#'   }
 #' @param spatial_object an object of class "Spatial".
-#' @param epsg_new an EPSG code provided as character string.
-#' @param epsg_old optional, an EPSG code provided as character string.
+#' @param epsg_new an EPSG code provided as character string indicating the
+#'   desired projection for the returned object.
+#' @param epsg_old optional, an EPSG code provided as character string
+#'  specifying the projection before transformation if no proj4string is
+#'  supplied with the object.
 #'
 #' @return an object of class "Spatial".
 #' @export
-new_epsg <- function(spatial_object, epsg_new, epsg_old = NULL){
+transform_projection <- function(spatial_object, epsg_new, epsg_old = NULL){
   #add argument checks here - how to test for S4 class "Spatial"?
+  stopifnot(is.character(epsg_new) &&
+              (is.character(epsg_old) || is.null(epsg_old)))
 
   if(!is.null(epsg_old)){
     proj4string(spatial_object) <- CRS(paste0("+init=epsg:", epsg_old))
