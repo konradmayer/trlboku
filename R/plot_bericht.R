f3ba92acb778c851203fbc7ead35a42cb1de0c6c
konradmayer <konrad.mayer@boku.ac.at>
2016-10-20 09:38:01 +0200

    add lookup of species in data/species.Rdata to plot_bericht()

--- R/plot_bericht.R
+++ R/plot_bericht.R
@@ -1,12 +1,14 @@
 plot_bericht <- function(filename, encoding = 'WINDOWS-1252', set_lwd = 50,
                          multi = 0.7) {
   #create lookup table for allowed species and their assigned colors
-  baumarten <- data.frame(Baumart=c('Fichte', 'Tanne', 'L?rche', 'Eiche', 'Zirbe',
-                                    'Kiefer', 'Nadelholz', 'Ulme', 'Buche'),
-                          Farbe=c('#056608', '#0018A8', '#D3212D', '#967117', '#FFEF00',
-                                  '#FF7E00', '#9FA91F', '#5e245e', '#b70505'),
-                          stringsAsFactors = FALSE)
-  rownames(baumarten) <- baumarten[ ,1]
+  # species <- data.frame(Baumart=c('Fichte', 'Tanne', 'L?rche', 'Eiche', 'Zirbe',
+  #                                   'Kiefer', 'Nadelholz', 'Ulme', 'Buche'),
+  #                         Farbe=c('#056608', '#0018A8', '#D3212D', '#967117', '#FFEF00',
+  #                                 '#FF7E00', '#9FA91F', '#5e245e', '#b70505'),
+  #                         stringsAsFactors = FALSE)
+
+  data('species')
+  rownames(species) <- species[ ,'german']
 
   #read in files
   header <- gdata::read.xls (filename, sheet = 1, header = F,
@@ -18,8 +20,8 @@ plot_bericht <- function(filename, encoding = 'WINDOWS-1252', set_lwd = 50,
   columns <- c(2, 3, 4, 5, 7)
   df[,columns] <- lapply(df[ ,columns], FUN = function(x) as.character(x))
 
-  #test if all species in "Baumart" are specified within the object baumarten
-  if(any(!(df[ , 3] %in% baumarten[,1]))){
+  #test if all species in "Baumart" are specified within the object species
+  if(any(!(df[ , 3] %in% species[,'german']))){
     stop('typing error in "Baumart" or species not implemented')
   }
 
@@ -55,59 +57,61 @@ plot_bericht <- function(filename, encoding = 'WINDOWS-1252', set_lwd = 50,
   #compiling data set for plotting
   dat <- data.frame(df[2:3], date.begin, date.end , df[6], Farbe = NA, df[5],
                     unmeasured)
-  dat[ ,6] <- baumarten[dat[, 'Holzart'], 2]
+  dat[ ,6] <- species[dat[, 'Holzart'], 'color']
 
   dat <- dat[order(dat$letztes.Jahr+dat$unmeasured), ]
 
   #PLOT----------------------
+  makeplot <- function() {
+    xlim <- c(min(dat[3]) - 10, max(dat[ , 4] + dat[ , 8], na.rm = TRUE) + 10)
+    ylim <- c(0, nrow(dat) + 1)
 
-  xlim <- c(min(dat[3]) - 10, max(dat[ , 4] + dat[ , 8], na.rm = TRUE) + 10)
-  ylim <- c(0, nrow(dat) + 1)
+    png(paste(header[1, 2], '.png', sep = ""), units = 'in',
+        height = (ylim[2] + 2) * multi, width = 12, res = 150)
 
-  png(paste(header[1, 2], '.png', sep = ""), units = 'in',
-      height = (ylim[2] + 2) * multi, width = 12, res = 150)
+    par(mai = c(1, 1, 0.4, 1))
 
-  par(mai = c(1, 1, 0.4, 1))
+    plot(0, xlim = xlim, main = header[1, 2], type = 'l', bty = 'o', yaxt = 'n',
+         ylim = ylim, ylab = 'Probe', xlab = 'Jahr', lwd = set_lwd, xaxs = 'i',
+         yaxs = 'i')
 
-  plot(0, xlim = xlim, main = header[1, 2], type = 'l', bty = 'o', yaxt = 'n',
-       ylim = ylim, ylab = 'Probe', xlab = 'Jahr', lwd = set_lwd, xaxs = 'i',
-       yaxs = 'i')
+    grid(ny = NA, col = 'grey50')
 
-  grid(ny = NA, col = 'grey50')
-
-  axis(2, at = seq_len(nrow(dat)), labels = dat[ ,1], las = 1)
-  axis(4, at = seq_len(nrow(dat)),
-       labels = dat[ , 4] + dat[ , 8], las = 1, outer = F)
-
-
-  #plot not measured rings
-  these_lines <- which(!is.na(dat[ ,8]))
-  lapply(these_lines, FUN = function(i) {
-    segments(dat[i, 4], i, dat[i, 4] + dat[i, 8],  col = 'grey90',
-             lend = 1,lwd = set_lwd, xaxs = 'i', yaxs = 'i')
-  })
-
-  #plot segments
-  lapply(seq_len(nrow(dat)), FUN = function(i) {
-    lines(as.numeric(dat[i, 3:4]), c(i,i), lwd = set_lwd, lend = 3,
-          col = dat[i, 6], xaxs = 'i', yaxs = 'i')
-    #text(xlim[2] + 0.5, i, dat[i, 4] + dat[i, 8], pos = 4)
-  })
+    axis(2, at = seq_len(nrow(dat)), labels = dat[ ,1], las = 1)
+    axis(4, at = seq_len(nrow(dat)),
+         labels = dat[ , 4] + dat[ , 8], las = 1, outer = F)
 
 
-  #waldkante
-  these_lines <- which(dat[ ,7] == 'ja')
-  lapply(these_lines, FUN = function(i) {
-    points(dat[i, 4] + dat[i, 8] + 2, i, pch = 20)
-    lines(as.numeric(c(dat[i, 4] + dat[i, 8] - 1,dat[i, 4] + dat[i, 8])), c(i, i),
-          lwd = set_lwd, lend = 1, col = 'black', xaxs = 'i', yaxs = 'i')
-  })
+    #plot not measured rings
+    these_lines <- which(!is.na(dat[ ,8]))
+    lapply(these_lines, FUN = function(i) {
+      segments(dat[i, 4], i, dat[i, 4] + dat[i, 8],  col = 'grey90',
+               lend = 1,lwd = set_lwd, xaxs = 'i', yaxs = 'i')
+    })
+
+    #plot segments
+    lapply(seq_len(nrow(dat)), FUN = function(i) {
+      lines(as.numeric(dat[i, 3:4]), c(i,i), lwd = set_lwd, lend = 3,
+            col = dat[i, 6], xaxs = 'i', yaxs = 'i')
+      #text(xlim[2] + 0.5, i, dat[i, 4] + dat[i, 8], pos = 4)
+    })
 
 
-  #legend
-  legende <- baumarten[unique(dat$Holzart),]
-  legend('bottom', legend = legende[, 1], fill = as.character(legende[, 2]),
-         ncol = nrow(legende), bty = 'n')
+    #waldkante
+    these_lines <- which(dat[ ,7] == 'ja')
+    lapply(these_lines, FUN = function(i) {
+      points(dat[i, 4] + dat[i, 8] + 2, i, pch = 20)
+      lines(as.numeric(c(dat[i, 4] + dat[i, 8] - 1,dat[i, 4] + dat[i, 8])), c(i, i),
+            lwd = set_lwd, lend = 1, col = 'black', xaxs = 'i', yaxs = 'i')
+    })
 
-  dev.off()
+
+    #legend
+    legende <- species[unique(dat$Holzart),]
+    legend('bottom', legend = legende[, 'german'], fill = as.character(legende[, 'color']),
+           ncol = nrow(legende), bty = 'n')
+
+    dev.off()
+  }
+  try(makeplot())
 }
