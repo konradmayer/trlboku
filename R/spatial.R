cfa1623dc08df57824090a835788d0587d0026b1
konradmayer <konrad.mayer@boku.ac.at>
2016-10-25 12:44:55 +0200

    add example and unit test to transform_projection()

--- R/spatial.R
+++ R/spatial.R
@@ -1,5 +1,5 @@
 #------------------------------
-#other_projection
+#transform_projection
 #------------------------------
 #' @title transform_projection
 #' @description Transforms to different projection by supplying an EPSG code.
@@ -20,16 +20,40 @@
 #'
 #' @return an object of class "Spatial".
 #' @export
+#' @examples \dontrun{
+#' #library('sp')
+#' init_dataframe <- data.frame(Y = c(48.6476, 48.6447, 48.6709),
+#'                              X = c(16.7958, 6.7962, 16.5370),
+#'                              meta = c('a', 'b', 'c'))
+#' spatpoints <- SpatialPointsDataFrame(init_dataframe[c('X', 'Y')],
+#'                                        init_dataframe['meta'])
+#'
+#' ## using other_projection():
+#' spatpoints_lambert <- transform_projection(spatial_object, '31287',
+#' epsg_old = '4326')
+#'
+#' ## using "manual approach":
+#' # specify current projstring as WGS 84
+#' proj4string(spatpoints) <- CRS("+init=epsg:4326")
+#' # creator function for CRS class - set as Lambert-projection Austria
+#' CRS.new <- CRS("+init=epsg:31287")
+#' #transform spatialpointsto new projection
+#' spatpoints_lambert2 <- spTransform(spatpoints, CRS.new)
+#'
+#' #they are the same
+#' identical(spatpoints_lambert, spatpoints_lambert2)
+#' }
+
 transform_projection <- function(spatial_object, epsg_new, epsg_old = NULL) {
   #add argument checks here - how to test for S4 class "Spatial"?
   stopifnot(is.character(epsg_new) &&
               (is.character(epsg_old) || is.null(epsg_old)))
 
   if(!is.null(epsg_old)) {
-    proj4string(spatial_object) <- CRS(paste0("+init=epsg:", epsg_old))
+    sp::proj4string(spatial_object) <- sp::CRS(paste0("+init=epsg:", epsg_old))
   }
 
-  CRS.new <- CRS(paste0("+init=epsg:", epsg_new))
-  out <- spTransform(spatial_object, CRS.new)
+  CRS.new <- sp::CRS(paste0("+init=epsg:", epsg_new))
+  out <- sp::spTransform(spatial_object, CRS.new)
   return(out)
 }
---
+++ tests/testthat/test-transform_projection.R
@@ -0,0 +1,21 @@
+context('transform projection')
+
+init_dataframe <- data.frame(Y = c(48.6476, 48.6447, 48.6709),
+                             X = c(16.7958, 6.7962, 16.5370),
+                             meta = c('a', 'b', 'c'))
+
+spatpoints <- sp::SpatialPointsDataFrame(init_dataframe[c('X', 'Y')], init_dataframe['meta'])
+
+#function
+spatpoints_lambert <- transform_projection(spatpoints, '31287',
+                                               epsg_old = '4326')
+
+#manual
+sp::proj4string(spatpoints) <- sp::CRS("+init=epsg:4326")
+CRS.new <- sp::CRS("+init=epsg:31287")
+spatpoints_lambert2 <- sp::spTransform(spatpoints, CRS.new)
+
+test_that('transform_projection has same output as manual approach', {
+  expect_that(identical(spatpoints_lambert, spatpoints_lambert2), is_true())
+})
+
