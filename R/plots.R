5a9e5a351a598d88d1710d0c48790087b21bf19e
konradmayer <konrad.mayer@boku.ac.at>
2016-11-21 10:39:24 +0100

    add diagonal panel to add histograms to pairs()

--- R/plots.R
+++ R/plots.R
@@ -156,3 +156,20 @@ error_bar <- function(x, y, ll, ul, width) {
   lines(c(x - w, x + w), rep(ul, 2))
   lines(c(x - w, x + w), rep(ll, 2))
 }
+
+#panel.hist---------------------------------------------------------------------
+#' @title Add a histogram to diagonal panel in pairs()
+#' @param x vector
+#' @param ... additional arguments
+#' @export
+#' @examples
+#' pairs(iris, diag.panel = panel.hist)
+panel.hist <- function(x, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col = "gray80", ...)
+}
