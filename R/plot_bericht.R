11e9792199a48d44fa40ca53e011063a392a1f85
konradmayer <konrad.mayer@boku.ac.at>
2016-12-19 12:09:22 +0100

    Update plot_bericht.R

--- R/plot_bericht.R
+++ R/plot_bericht.R
@@ -10,9 +10,9 @@
 #' @export
 #' @example 
 #' #use the following line to select the file interactively:
-#' ## Not run:
+#' \dontrun{
 #' plot_bericht(file.choose())
-#' ## End(**Not run**)
+#' }
 
 
 plot_bericht <- function(filename, encoding = 'WINDOWS-1252', set_lwd = 50,
