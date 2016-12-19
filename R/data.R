a7f1ef18de3ddbf774bc9f1f591168b2e72a748c
konradmayer <konrad.mayer@boku.ac.at>
2016-12-19 11:18:45 +0100

    change typos in species

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #' species
 #'
 #' common species in dendrochronology in central europe, their abbriviations
-#' used in the tree ring lab at boku and consistently used colors for plotting.
+#' used in the tree ring lab at BOKU and consistently used colors for plotting.
 #'
 #' @format A data.frame with 18 rows and 6 variables:
 #' \describe{
@@ -10,7 +10,7 @@
 #'   \item{german}{species name in german}
 #'   \item{english}{species name in english}
 #'   \item{idshort}{species id used at stands with more than 99 series}
-#'   \item{idcolor}{species id used at stands with less than 99 series}
+#'   \item{idlong}{species id used at stands with less than 99 series}
 #'   \item{color}{color used for plotting}
 #'   }
 #'
