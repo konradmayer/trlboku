f686fe3211eacf4a3459e201ddb4b24fad152b77
konradmayer <konrad.mayer@boku.ac.at>
2018-10-02 14:43:17 +0200

    fix typo and make truncate_rwl() faster

--- tests/testthat/test-truncate_rwl.R
+++ tests/testthat/test-truncate_rwl.R
@@ -4,6 +4,8 @@ n <- 15
 dat <-  within(data.frame(row.names = seq.int(1601, length.out = n)),
                {x = c(rep(NA, 3), runif(n - 5), rep(NA, 2));
                y = c(rep(NA, 2), runif(n - 5), rep(NA, 3))})
+dat2 <- dat
+dat2[4:6, ] <- NA
 
 out <- dat[3:13, ]
 
@@ -14,6 +16,14 @@ test_that('truncate_rwl works as expected', {
 
 test_that('truncate_rwl handles data.frames with one column', {
   expect_that(truncate_rwl(dat[1]), equals(dat[as.character(1603:1612), 1, drop=FALSE]))
+})
+
+test_that('truncate_rwl only truncates at the upper and lower end but keeps gaps within the file', {
+  expect_that(truncate_rwl(dat2), equals(dat2[as.character(1603:1613), , drop=FALSE]))
+})
+
+test_that('truncate_rwl handles dataframes with only NAs', {
+  expect_that(truncate_rwl(dat2[4:6, ]), equals(data.frame()))
 })
 
 test_that('truncate_rwl handles input of wrong class', {
