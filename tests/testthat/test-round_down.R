061f8e857be86864fedd87b157784cc250d2e694
konradmayer <konrad.mayer@boku.ac.at>
2019-10-15 13:18:58 +0200

    make sure package installs and passes tests. deprecated testthat functions changed.

--- tests/testthat/test-round_down.R
+++ tests/testthat/test-round_down.R
@@ -15,7 +15,7 @@ test_that('round_down manages negative numbers', {
 test_that('round_down gives correct output with wrong input', {
   expect_that(round_down('a', 10), throws_error('non-numeric argument to binary operator'))
   expect_that(round_down(NULL, 10), equals(numeric(0)))
-  expect_that(is.na(round_down(NA, 10)), is_true())
+  expect_true(is.na(round_down(NA, 10)))
   expect_that(round_down(as.factor(1), 10), throws_error('input must be numeric'))
 })
 
@@ -25,7 +25,7 @@ test_that('round_down works for vectors', {
 
 
 test_that('round_down handles wrong input for to', {
-  expect_that(is.na(round_down(1, NA)), is_true())
+  expect_true(is.na(round_down(1, NA)))
   expect_that(round_down(1, 'a'), throws_error('non-numeric argument to binary operator'))
   expect_that(round_down(1, NULL), throws_error('input must be numeric'))
   expect_that(round_down(1, as.factor(1)), throws_error('input must be numeric'))
